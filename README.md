424338cdadcd7bf8123d12db1b7d5f318fb6bb8c
William Granados <william.granados@wgma00.me>
2016-09-01 23:58:03 -0400

    added baseframework for accessing google spreadsheets

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # GmailBot
-This bot will be used for keeping track of individual progress on a spreadsheet
+This bot will be used for keeping track of individual progress on a spreadsheet.
+It will take emails from an email account and parse for emails specifically forwarded
+from open kattis
+
+# Setting up
+####Python version:
+- 3.0 and up 
+
+### Guide:
+1. Clone the git repo to your desired location 
+2. Use ``pip install -r requirements.txt`` to install dependecies
+3. Make sure you have recieved OAuth2 credentials from google, use the following
+   as a guide: http://gspread.readthedocs.io/en/latest/oauth2.html 
+4. Remember to share the spreadsheet with the email proved from the OAuth2
+   process
+5. Assuming you're on linux use do the following command in the terminal:
+   ``cp details-example.yaml details.yaml`` then edit the ``details.yaml`` file
+   as the detailed instructions inquire.
+6. Make sure that you have shared 
+7. Run using ``python3 app.py``
+
+# Lisence
+
+This is distributed under the terms of the [MIT License][1].
+   [1]: https://github.com/csecutsc/GmailBot/blob/master/LICENSE 
---
+++ app.py
@@ -0,0 +1,69 @@
+# The MIT License (MIT)
+# 
+# Copyright (c) 2016 Computer Science Enrichment Club 
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in 
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import gspread
+import yaml
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+class SpreadSheetTracker(object):
+    """ Handles getting user data and uploading to a google spreadsheet.
+
+    Uses the google sheets api to access and upload data.
+
+    Attributes:
+        details: map, maps variables to sensitive information.
+        torn_api_key: string, api key provided by torn.
+        google_creds_path: string, path to the credentials i.e. "*.json".
+        google_sheets_key: string, code given to a google spreadsheet.
+        wks: worksheet object, object representing the online spreadsheet.
+    """
+    def __init__(self):
+        """Initializes basic contents required to access the service."""
+        with open("details.yaml", 'r') as yaml_file:
+           self.details = yaml.load(yaml_file)
+           self.google_creds_path = str(self.details['google_creds_path'])
+           self.google_sheets_key = str(self.details['google_sheets_key'])
+           self.user_emails = self.details['user_emails']
+           self.problem_ids = self.details['problem_ids']
+           self.wks = None
+           self.googleSpreadSheetSetup()
+
+    def googleSpreadSheetSetup(self):
+        """Authenticates user into google services and accesses spreadsheet."""
+        self.scope = ['https://spreadsheets.google.com/feeds']
+        self.credentials = ServiceAccountCredentials.from_json_keyfile_name(self.google_creds_path, self.scope)
+        self.gc = gspread.authorize(self.credentials)
+        self.wks = self.gc.open_by_key(self.google_sheets_key).sheet1
+
+    def modifySpreadSheet(self):
+        """Modifies spreadsheet"""
+        # please refer to https://github.com/burnash/gspread for api details
+        # related to modifying spreadsheets
+        pass
+
+
+
+if __name__ == '__main__':
+    sst = SpreadSheetTracker()
+    sst.modifySpreadSheet()
+
